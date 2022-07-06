1da6ce176393de2bbd69c80ec1b23a7bb02c3be4
PikichGleb <PikichGleb@yandex.ru>
2022-07-07 00:17:47 +0300

    Added Ex_005_ConditionIfElse

---
+++ Ex_005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write(" Введите имы пользователя: ");
+string username = Console.ReadLine();
+if( username.ToLower() == "цаца" ) // в "" писать с маленькой буквы, и не ставть перед/после написания в ковычках пробел 
+    {
+        Console.WriteLine( " Здравствуй Цаца новый год! " );
+    }   
+    else
+    {
+        Console.Write( " Привет, " );
+        Console.WriteLine( username );
+    }
