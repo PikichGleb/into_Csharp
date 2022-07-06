2a2f1486a4f0cc6fa0bf834a3489e32bd178b71c
PikichGleb <PikichGleb@yandex.ru>
2022-07-06 22:15:49 +0300

    Added Ex_001

--- Ex_001/Program.cs
+++ Ex_001/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Комментарий
+Console.WriteLine("Hello, World! -_- ");
+Console.WriteLine("Hello, World! -_- ");
+Console.WriteLine("Hello, World! -_- ");
