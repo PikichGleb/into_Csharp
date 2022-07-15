33806d9f7bb69abbc9201b6c46d07685ecbba727
PikichGleb <PikichGleb@yandex.ru>
2022-07-15 05:03:27 +0300

    Lecture 2(Ex_008 - Ex_011)

---
+++ Ex_010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//Поиск числа в массиве и вывод его индекса
+
+int[] array = { 1, 12, 31, 4, 33, 76, 84, 33 };
+int n = array.Length;
+
+int find = 33;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //Прервись
+    }
+    index++;
+}
---
+++ Ex_011_ArrayLibrary/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+    void FillArray(int[] collection)
+    {
+        int lenght = collection.Length;
+        int index = 0;
+        while (index < lenght)
+        {
+            collection[index] = new Random().Next(1, 10);
+            index++;
+        }
+    }
+
+    void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while(position < count){
+            Console.WriteLine(col[position]); // Вывод массива, с новой строки
+            position++;
+        }
+    }
+
+
+    int[] array = new int[10]; //Создание нового массива в котором будет 10 элементов. По умалчанию наполнен 0.
+
+    FillArray(array);
+    PrintArray(array);
+*/
+
+
+void FillArray(int[] collection) // Заполнение массива случайными числами
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // Вывод массива, в строку через пробел
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //Создание нового массива в котором будет 10 элементов. По умалчанию наполнен 0.
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
